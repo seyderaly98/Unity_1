bece58d2c5bead4784bc86192ab82a4d6c34bc1b
Almat <seyderaly98@gmail.com>
2021-07-02 14:55:53 +0600

    init

---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+    Character character;
+    public GameObject target;
+    Character characterTarget;
+
+    void Start()
+    {
+        character = GetComponentInParent<Character>();
+        characterTarget = target.GetComponent<Character>();
+    }
+
+    void ShootEnd()
+    {
+        character.SetState(Character.State.Idle);
+    }
+
+    void AttackEnd()
+    {
+        character.SetState(Character.State.RunningFromEnemy);
+    }
+
+    void DeathTarget()
+    {
+        if (!characterTarget.death)
+        {
+            characterTarget.animator.SetTrigger("DeathTarget");
+            characterTarget.death = true;
+        }
+    }
+}
